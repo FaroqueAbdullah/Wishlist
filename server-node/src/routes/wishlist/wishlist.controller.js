89b646f4372336b7bdc007b25cb25013a308190a
Faroque Abdullah <abdullahabir3@gmail.com>
2022-05-17 02:59:35 +0600

    [FE] - User wish list

--- server-node/src/routes/wishlist/wishlist.controller.js
+++ server-node/src/routes/wishlist/wishlist.controller.js
@@ -1,5 +1,50 @@
 const Wishlist = require('../../models/wishlist.model');
 
+const checkUserWishlist = async ( userID ) => {
+  try {
+    return await Wishlist.findOne({ 
+      userId: userID
+    });
+  } catch (err) {
+    return res.status(500).send({ 'message' : "Server error" });
+  }
+}
+
+const checkProductOnUserWishlist = async ( userId, productId ) => {
+  try {
+    return await Wishlist.findOne({
+      userId: userId,
+      products: { $elemMatch: { productId } }
+    });
+  } catch (err) {
+    return res.status(500).send({ 'message' : "Server error" });
+  }
+}
+
+const createWishlist = async ( userId, product ) => {
+  try {
+    console.log('createOrUpdateWishlist call')
+    return await Wishlist.create(
+      { userId: userId,
+        products: [product] 
+      });
+  } catch (err) {
+    console.log('createOrUpdateWishlist error')
+    return res.status(500).send({ 'message' : "Server error" });
+  }
+}
+
+const updateWishList = async ( req, res, next ) => {
+  try {
+    return await Wishlist.updateOne(
+      { userId: userId }, 
+      { $push: { products: product } 
+    },{upsert: true})
+  } catch (err) {
+    return res.status(500).send({ 'message' : "Server error" });
+  }
+}
+
 const getWishlist = async ( req, res, next ) => {
   try {
     const WishlistById = await Wishlist.findOne({ 
@@ -17,29 +62,40 @@ const getWishlist = async ( req, res, next ) => {
 }
 
 const addWishlist = async ( req, res, next ) => {
+  
   try {
     const { productId } = req.body;
 
-    const checkWishlistById = await Wishlist.findOne({
-      userId: res.user_id,
-      products: { $elemMatch: { productId } }
-    });
+    const checkWishlistByUserId = await checkUserWishlist( res.user_id )
 
-    if (checkWishlistById) {
+    if ( !checkWishlistByUserId ) {
+      const wishlist = await createWishlist( res.user_id, req.body );
+    
+      return res.status(200).send({ 
+        'status': 'success',
+        'message' : "Product added to the wishlist",
+        'data': wishlist 
+      });
+    }
+
+    const checkWishlistByProductId = await checkProductOnUserWishlist( res.user_id, productId );
+
+    if (checkWishlistByProductId) {
       return res.status(409 ).send({ 
         'status': 'error',
         'message' : "Product already exists in wishlist"
       });
     }
 
-    const wishlist = await Wishlist.updateOne(
-      { userId: res.user_id }, 
-      { $push: { products: req.body  } 
-    },{upsert: true})
+    const wishlist = await updateWishList( res.user_id, req.body );
     
-    return res.status(200).send({ "data": req.body });
+    return res.status(200).send({ 
+      'status': 'success',
+      'message' : "Product added to the wishlist",
+      'data': wishlist 
+    });
   } catch (err) {
-    return res.status(500).send({ 'message' : "Server error" });
+    return res.status(500).send({ 'message' : err });
   }
 }
 
@@ -65,7 +121,11 @@ const deleteWishlist = async ( req, res, next ) => {
       { $pull: { products: { productId }} 
     },{safe: true, multi:true})
     
-    return res.status(200).send({ "message": "Product deleted from the wishlist" });
+    return res.status(200).send({ 
+      'status': 'success',
+      "message": "Product deleted from the wishlist",
+      'data': wishlist
+    });
   } catch (err) {
     return res.status(500).send({ 'message' : "Server error" });
   }
